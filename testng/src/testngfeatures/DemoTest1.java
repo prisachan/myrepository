5432ca1276acc1f09cd4d17d7e9a831a0089e67b
hp <hp@hp-PC>
2018-07-21 01:03:12 +0530

    2nd push

---
+++ testng/src/testngfeatures/DemoTest1.java
@@ -0,0 +1,28 @@
+package testngfeatures;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+	@Test
+	public void test1(){
+		System.out.println("test1");
+		Assert.assertEquals(true, true);
+	}
+	@Test
+	public void test2(){
+		System.out.println("test2");
+		Assert.assertEquals(true, true);	
+	}
+	@Test
+	public void test3(){
+		System.out.println("test3");
+		Assert.assertEquals(true, true);
+	}
+	@Test
+	public void test4(){
+		System.out.println("test4");
+		Assert.assertEquals(true, true);
+	}
+
+}
